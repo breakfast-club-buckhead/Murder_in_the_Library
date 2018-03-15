d3ab34e18bb7d91ebe5a015d0699f7a1097d17b5
Toby Ho <airportyh@gmail.com>
2018-03-15 13:38:25 -0400

    Update chapter_1.py

--- chapter_1.py
+++ chapter_1.py
@@ -2,7 +2,7 @@ print("One late Thursday morning, as they gathered at the haunted pizza place,")
 print ("there was a skull sitting on the counter, humming.")
 
 
-print'(But let me tell you a story,  which you might not believe. We will codes,  letters and numbers')
+print('But let me tell you a story,  which you might not believe. We will codes,  letters and numbers')
 print('gives us characters which may set our mind at ease..... or maybe ...........')
 
 
