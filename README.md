01948952ce22b480ce4955425805b7fff66d57ee
Faith Wallace <fwallacephd@gmail.com>
2018-03-15 12:53:21 -0400

    changes by Faith 12:52pm

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Murder_in_the_Library
 
 This is a novel written in Python. It will sell millions.
+It will be narrated by Will Wheaton.
--- chapter_1.py
+++ chapter_1.py
@@ -1,1 +1,2 @@
 print("One late Thursday morning, as they gathered at the haunted pizza place,")
+print ("There was a skull sitting on the counter, humming.")
