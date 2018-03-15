39ded61bbe2536d88b681b8381ff49a7fe31c933
Nerajno <nerajno@gmail.com>
2018-03-15 13:07:14 -0400

    testing"

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 # Murder_in_the_Library
 
 This is a novel written in Python. It will sell millions.
-
-But let me tell you a story,  which you might not believe. We will codes,  letters and numbers
-gives us characters which may set our mind at ease..... or maybe ...........
-
-'HI THERE' .. yes you .... you with the nostril hair..... hhmmmm... yes you.... Deadpool here.
--- chapter_1.py
+++ chapter_1.py
@@ -1,8 +1,18 @@
 print("One late Thursday morning, as they gathered at the haunted pizza place,")
 
 
-But let me tell you a story,  which you might not believe. We will codes,  letters and numbers
-gives us characters which may set our mind at ease..... or maybe ...........
+print'(But let me tell you a story,  which you might not believe. We will codes,  letters and numbers')
+print('gives us characters which may set our mind at ease..... or maybe ...........')
 
-'HI THERE' .. yes you .... you with the nostril hair..... hhmmmm... yes you.... Deadpool here..
-Nice novel so far, right ?
+
+
+
+
+
+
+
+
+
+
+print('HI THERE .. yes you .... you with the nostril hair..... hhmmmm... yes you.... Deadpool here..')
+print('Nice novel so far, right ?')
