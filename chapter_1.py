19219842e1a8aa02b9ee5959a96dd35524df5230
Nerajno <nerajno@gmail.com>
2018-03-15 14:10:39 -0400

    still testing

--- chapter_1.py
+++ chapter_1.py
@@ -10,5 +10,5 @@ print('HI THERE .. yes you .... you with the nostril hair..... hhmmmm... yes you
 print('Nice novel so far, right ?')
 
 print('Another line ')
-print('test')
+print('test1')
 print('test')
