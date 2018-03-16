1dbf5e11fa0715f9c55215eaa661da886ad1257c
Faith Wallace <fwallacephd@gmail.com>
2018-03-16 11:09:23 -0400

    faith's updates 031618

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Murder_in_the_Library
+Murder_in_the_Library
 
 This is a novel written in Python. It will sell millions.
 It will be narrated by Will Wheaton.
+Michael B. Jordan will star in the movie version.
--- chapter_1.py
+++ chapter_1.py
@@ -2,10 +2,12 @@ print("One late Thursday morning, as they gathered at the haunted pizza place,")
 print ("there was a skull sitting on the counter, humming.")
 
 
-print'(But let me tell you a story,  which you might not believe. We will codes,  letters and numbers')
-print('gives us characters which may set our mind at ease..... or maybe ...........')
+print ("The skull stopped its song and said, 'Let me tell you a story,  which you might not believe. It has codes embedded in letters and numbers.)
+print('And I will tell you about characters, which may set our mind at ease..... or maybe ...........')
 
 
 print('HI THERE .. yes you .... you with the nostril hair..... hhmmmm... yes you.... Deadpool here..')
 print('Nice novel so far, right ?')
+name = input("What's your name anyway?")
+print ("Okay, ", name, "get ready for a real whopper of a tale...")
 
